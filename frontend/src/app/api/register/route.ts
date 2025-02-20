e60856b4831fbcc02adeed3df845cd6cc185647a
Wil Hargrave <willhargrave@hotmail.co.uk>
2025-02-20 14:18:59 +0900

    logged in Users can delete an RSS feed

--- frontend/src/app/api/register/route.ts
+++ frontend/src/app/api/register/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
         id: uuidv4(),
         username,
         password: hashedPassword,
-        feeds: [],
+        removedFeeds: [],
       },
     });
 
