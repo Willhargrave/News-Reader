cb43fe60ff265f6aa4f3e5fc72607fcbb63c2edd
Wil Hargrave <willhargrave@hotmail.co.uk>
2025-03-05 14:57:51 +0900

    added transition

--- frontend/src/components/ArticleList.tsx
+++ frontend/src/components/ArticleList.tsx
@@ -1,37 +1,47 @@
-import { ArticleListProps } from "@/types";
+"use client"
+import { Transition } from "@headlessui/react";
+import { ArticleListProps } from "../types";
+
 export default function ArticleList({ articles, linksVisible, loading }: ArticleListProps) {
-  
-    if (loading) {
-        return (
-          <div className="flex justify-center items-center py-10">
-            <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
-          </div>
-        );
-      }
-  
+  if (loading) {
     return (
+      <div className="flex justify-center items-center py-10">
+        <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    );
+  }
+
+  return (
     <div>
-      <h2 className="text-2xl font-bold mb-4">Top News Stories</h2>
       {articles.map((article, index) => (
-        <div key={index} className="mb-6">
-          <h3 className="text-xl font-bold">
-            {article.feedTitle}: {article.headline}
-          </h3>
-          <p>{article.content}</p>
-          {linksVisible && (
-            <p className="mt-2">
-              <a
-                href={article.link}
-                target="_blank"
-                rel="noreferrer"
-                className="text-blue-500 underline"
-              >
-                Read more
-              </a>
-            </p>
-          )}
-          <hr className="mt-4 border-t border-gray-300" />
-        </div>
+        <Transition
+          key={index}
+          appear={true}
+          show={true}
+          enter="transition-opacity duration-700"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+        >
+          <div className="mb-6">
+            <h3 className="text-xl font-bold">
+              {article.feedTitle}: {article.headline}
+            </h3>
+            <p>{article.content}</p>
+            {linksVisible && (
+              <p className="mt-2">
+                <a
+                  href={article.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-blue-500 underline"
+                >
+                  Read more
+                </a>
+              </p>
+            )}
+            <hr className="mt-4 border-t border-gray-300" />
+          </div>
+        </Transition>
       ))}
     </div>
   );
