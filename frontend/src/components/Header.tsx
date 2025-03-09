1ef4a9164e9d783106ce1615bb239d78d663f61b
Wil Hargrave <willhargrave@hotmail.co.uk>
2025-03-09 22:13:18 +0900

    added light/dark mode

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,24 @@
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
-
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
 export default function Header() {
-
 const { data: session } = useSession();
+const {theme, setTheme} = useTheme()
+const [mounted, setMounted] = useState(false)
+
+useEffect(() => setMounted(true), [])
 
 return(
 <header className="flex justify-between items-center mb-6">
 <h1 className="text-3xl font-bold">Just The News</h1>
-<div>
+<button
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      className="px-4 py-2 border rounded"
+    >
+      {mounted ? `Toggle ${theme === 'dark' ? 'Light' : 'Dark'} Mode` : 'Toggle Mode'}
+    </button>
   {!session ? (
     <div className="flex space-x-4">
       <Link
@@ -38,7 +47,6 @@ return(
       </button>
     </div>
   )}
-</div>
 </header>
 )
 }
