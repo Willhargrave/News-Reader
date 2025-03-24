959d506c8a2cc578e1679c853e6da36e5bdf5df1
Wil Hargrave <willhargrave@hotmail.co.uk>
2025-03-24 15:38:00 +0900

    updated prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,6 @@ import NextAuth from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
 
-console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
-
-
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
