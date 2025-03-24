d0dabc70b9a06316cb8346bec45dc5b12c6ecb70
Wil Hargrave <willhargrave@hotmail.co.uk>
2025-03-24 17:44:12 +0900

    added prisma generate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query'] : []
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query'] : [],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL + '&connection_limit=5'
+      }
+    }
   });
+};
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
 
 export default prisma;
