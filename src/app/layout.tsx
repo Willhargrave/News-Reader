7bf559c0bb795c4e397867a4110dc7cf8a15065c
Wil Hargrave <willhargrave@hotmail.co.uk>
2025-03-25 13:28:22 +0900

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Providers from "./providers/Providers";
 import "./globals.css";
 import { ThemeProvider } from "@/app/providers/ThemeProvider";
 import { Inter } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
@@ -24,8 +24,11 @@ export default function RootLayout({
       <body 
       className={`${inter.className} ${inter.className} bg-background text-foreground antialiased transition-colors duration-500`}>
       <ThemeProvider>
-          <Providers>{children}</Providers>
+          <Providers>
+            {children}
+            </Providers>
       </ThemeProvider>
+      <Analytics/>
       </body>
     </html>
   );
